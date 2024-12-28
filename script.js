6de0530571998bee62c60ce69b79bdb18107a87e
Coder <111493707+informationabout@users.noreply.github.com>
2024-12-28 15:11:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+const codes = document.querySelectorAll('.code');
+
+codes.forEach((code, idx) => {
+    code.addEventListener('input', () => {
+        if (code.value && idx < codes.length - 1) {
+            codes[idx + 1].focus();
+        }
+    });
+
+    code.addEventListener('keydown', (e) => {
+        if (e.key === 'Backspace') {
+            if (!code.value && idx > 0) {
+                codes[idx - 1].focus();
+            }
+        }
+    });
+});
